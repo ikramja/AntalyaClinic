25dc1f2b3bbc4ee3dd381032304e284f9b04b9d8
ikramja <ikramboumediene05@gmail.com>
2023-05-03 20:20:15 +0100

    service page

--- src/Components/ImageBriefDescription/index.js
+++ src/Components/ImageBriefDescription/index.js
@@ -8,35 +8,37 @@ export default function ImageBriefDescription(props) {
 				className=" my-5 "
 				style={props.borderRight ? props.borderRight : {}}
 			>
-				<Row className="w-100">
-					<Col lg={5} className="px-0">
-						<img
-							className="image-Brief-description-image w-100 h-100"
-							src={props.source}
-						></img>
-					</Col>
-					<Col lg={7} className="px-1">
-						<div id={props.id} className="section">
-							<h1 className="image-brief-description-header mt-2 mb-0">
-								{props.title}
-							</h1>
+				<Container>
+					<Row className="w-100">
+						<Col lg={5} className="px-0">
+							<img
+								className="image-Brief-description-image w-100 h-100"
+								src={props.source}
+							></img>
+						</Col>
+						<Col lg={7} className="px-1">
+							<div id={props.id} className="section">
+								<h1 className="image-brief-description-header mt-2 mb-0">
+									{props.title}
+								</h1>
 
-							{props.subtitle && (
-								<div>
-									<br />
-									<h2 className="image-brief-description-subtitle pb-3">
-										{props.subtitle}
-									</h2>
-								</div>
-							)}
+								{props.subtitle && (
+									<div>
+										<br />
+										<h2 className="image-brief-description-subtitle pb-3">
+											{props.subtitle}
+										</h2>
+									</div>
+								)}
 
-							<div
-								className="image-Brief-description"
-								dangerouslySetInnerHTML={{ __html: props.description }}
-							/>
-						</div>
-					</Col>
-				</Row>
+								<div
+									className="image-Brief-description"
+									dangerouslySetInnerHTML={{ __html: props.description }}
+								/>
+							</div>
+						</Col>
+					</Row>
+				</Container>
 			</div>
 		</Container>
 	);
