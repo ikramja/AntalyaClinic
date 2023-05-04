91c315772909a6283d4a5ba78cff93a6e7866fb3
ikramja <ikramboumediene05@gmail.com>
2023-05-04 17:26:46 +0100

    article  page

--- src/Components/ImageBriefDescription/index.js
+++ src/Components/ImageBriefDescription/index.js
@@ -4,14 +4,14 @@ import "./index.css";
 export default function ImageBriefDescription(props) {
 	return (
 		<div className=" my-5 " style={props.borderRight ? props.borderRight : {}}>
-			<Row className="w-100 mx-5">
-				<Col lg={6} className="px-0">
+			<Row className="w-100 mx-5 ">
+				<Col lg={5} className="px-0">
 					<img
 						className="image-Brief-description-image w-100"
 						src={props.source}
 					></img>
 				</Col>
-				<Col lg={6} className="image-brief-description-text-column ">
+				<Col lg={7} className="image-brief-description-text-column ">
 					<div id={props.id} className="section px-5">
 						<h1 className="image-brief-description-header mt-2 mb-2 ">
 							{props.title}
