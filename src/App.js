d52e706609451e325fd15fa5cf09d91ac5221ea6
Jalil <benharkatdjalil@gmail.com>
2023-06-06 16:18:23 +0400

    adding comments

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import Footer from "./Layouts/Footer";
 import Main from "./Routes";
 import { useState } from "react";
 import VK from "react-vk";
+import "typeface-arsenal";
+import "typeface-montserrat";
+import "typeface-oswald";
+import { ConfigProvider } from "antd";
 
 function App() {
 	const [showContent, setShowContent] = useState(true);
+
 	return (
 		<div className="App">
-			<Header setShowContent={setShowContent} />
-			<VK apiId={51667816}>
-				<div className="content">{showContent && <Main />}</div>
-			</VK>
-			<Footer />
+			<ConfigProvider
+				theme={{
+					token: {
+						colorPrimary: "#016245",
+					},
+				}}
+			>
+				<Header setShowContent={setShowContent} />
+				<VK apiId={51667816}>
+					<div className="content">{showContent && <Main />}</div>
+				</VK>
+			</ConfigProvider>
 		</div>
 	);
 }
